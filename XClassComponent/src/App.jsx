b362b3dcb19a765d3e40277f77005ebd9462e427
anandabhijeet98 <anandabhijeet98@gmail.com>
2024-05-16 21:21:47 +0530

    Initial Push

---
+++ XClassComponent/src/App.jsx
@@ -0,0 +1,32 @@
+import { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <p>Count: {count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default App;
